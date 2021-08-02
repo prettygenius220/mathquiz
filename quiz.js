b8fcf67d73e045cd9e0b323aefecfabf1a4b4450
prettygenius220 <78955512+prettygenius220@users.noreply.github.com>
2021-08-02 23:03:31 +0100

    set to display right or wrong depending on user's inpute

--- quiz.js
+++ quiz.js
@@ -1,85 +1,78 @@
+// set the submit button to not display
 document.getElementById("submitDiv").style.display='none';
+// set the next button to not display
 document.getElementById("nextDiv").style.display='none';
+// set the question 1 div  to not display
 document.getElementById("ques").style.display='none';
+// set the question 2 div  to not display
 document.getElementById("ques2").style.display='none';
+// get question one
 var question1 = document.getElementById("question1");
+// get question two
 var question2 = document.getElementById("question2");
-// concerning the label for =
-var insertEqual = document.getElementById("equalto");
-// var equal = insertEqual.innerHTML = '=';
+// get the label 
+var equal = document.getElementById("equalto");
 // generate random numbers
+// first 
 const first = Math.floor(Math.random() * 10) + 1;
+// second
 const second = Math.floor(Math.random() * 10) + 1;
+// third
 const third = Math.floor(Math.random() * 10) + 1;
+// fourth
 const fourth = Math.floor(Math.random() * 10) + 1;
-// to extract the user's input
-var ans1 = document.getElementById("answer1st").value;
+
+// get wrong and right divs
+var wrong = document.getElementById("wrong");
+var right = document.getElementById("right");
+// hide wrong and right 
+wrong.style.display='none';
+right.style.display='none';
 
 
 
 
-// Displays the hidden divs
+// Displays the hidden divs when the start button is clicked
 function displayDivs(){
         document.getElementById("ques").style.display='block';
         document.getElementById("ques2").style.display='block';
         document.getElementById("startDiv").style.display='none';
         document.getElementById("nextDiv").style.display='block';
+        alert("If the computer returns 'Wrong' then one or both of your answers are wrong ")
         
 }
-
+// sets the first and second questions as the random numbers already derived
 function getNumbers(){
+        // question 1 is
         var combined1 = first + ' + ' + second + ' ' + '=' ;
+        // question 2 is
         var combined2 = third + ' + ' + fourth + '  ' + "=";
+        // set the inner value of the first question to...
         var firstques = question1.innerHTML = combined1;
+        // set the inner value of the second question to...
         var secondques = question2.innerHTML = combined2;
         
         
 }
 //  function to return the user's and computer's input
 function displayInput(){
-        var ansFirst = document.getElementById("answer1st").value; 
-        var ansSecond = document.getElementById("answer2nd").value;
+         // to get the user's input to the first answer
+         var ansFirst = document.getElementById("answer1st").value;
+         // to get the user's input to the second answer
+         var ansSecond = document.getElementById("answer2nd").value;
+        // to add the first and second random numbers together
         var sumRandomNumbers1 = first + second;
+        // to sum up the third and fourth random numbers
         var sumRandomNumbers2 = third + fourth;
-        var alertInput = alert ( " Computer's answer to question 1 = "+ sumRandomNumbers1 + "\n" +
-         " User's answer to question 2 = "  + ansFirst
-         + "\n \n Computer's answer to question 2 =" + sumRandomNumbers2
-         +"\n User's answer to question 2 = "+ ansSecond);
-        return alertInput;
+        // compare the user and the computer's input
+        if (ansFirst == sumRandomNumbers1 && ansSecond == sumRandomNumbers2){
+                right.style.display='block';
+                wrong.style.display='none';
+        } else {
+                wrong.style.display = 'block';
+                right.style.display = 'none';
+        }
         
 }
 
 
-// function determineScore(){
-        // var ans1 = document.getElementById("answer1st").value;
-        // var ans2 = document.getElementById("answer2nd").value;
-        // // var score = [];
-        // computer adds the figures
-        // var sumRandomNumbers = first + second;
-
-
-        // if ans1 result is 2 : append 1 to score 
-        // Otherwise : append 0 to score
-        // if (ans1 == 2) 
-        //         score.push(1);
-        
-        // else 
-        //         score.push(0);
-
-        // if (ans2 == 3)
-        //         score. push(1);
-        // else 
-        //         score.push(0);
-
-        // var total = 0;
-        // for (var i in score) {
-        //         total += score[i];
-        // }
-//         alert("Computer answer = "+ sumRandomNumbers + 
-//         "User answer = " + ans1);
-// }
-
-// // function to redirect to another page
-// function nextPage{
-//         location.
-// }
