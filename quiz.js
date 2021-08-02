5b30a7b3b45c62ef4e537b9236c4a5e64cce5386
prettygenius220 <78955512+prettygenius220@users.noreply.github.com>
2021-08-02 12:19:53 +0100

    files modified to display computer and user's answer

--- quiz.js
+++ quiz.js
@@ -12,6 +12,10 @@ const first = Math.floor(Math.random() * 10) + 1;
 const second = Math.floor(Math.random() * 10) + 1;
 const third = Math.floor(Math.random() * 10) + 1;
 const fourth = Math.floor(Math.random() * 10) + 1;
+// to extract the user's input
+var ans1 = document.getElementById("answer1st").value;
+
+
 
 
 // Displays the hidden divs
@@ -25,35 +29,54 @@ function displayDivs(){
 
 function getNumbers(){
         var combined1 = first + ' + ' + second + ' ' + '=' ;
-        var combined2 = third + ' + ' + third + '  ' + "=";
+        var combined2 = third + ' + ' + fourth + '  ' + "=";
         var firstques = question1.innerHTML = combined1;
         var secondques = question2.innerHTML = combined2;
         
         
 }
+//  function to return the user's and computer's input
+function displayInput(){
+        var ansFirst = document.getElementById("answer1st").value; 
+        var ansSecond = document.getElementById("answer2nd").value;
+        var sumRandomNumbers1 = first + second;
+        var sumRandomNumbers2 = third + fourth;
+        var alertInput = alert ( " Computer's answer to question 1 = "+ sumRandomNumbers1 + "\n" +
+         " User's answer to question 2 = "  + ansFirst
+         + "\n \n Computer's answer to question 2 =" + sumRandomNumbers2
+         +"\n User's answer to question 2 = "+ ansSecond);
+        return alertInput;
+        
+}
+
 
 // function determineScore(){
-//         var ans1 = document.getElementById("answer1st").value;
-//         var ans2 = document.getElementById("answer2nd").value;
-//         var score = [];
-//         // if ans1 result is 2 : append 1 to score 
-//         // Otherwise : append 0 to score
-//         if (ans1 == 2) 
-//                 score.push(1);
+        // var ans1 = document.getElementById("answer1st").value;
+        // var ans2 = document.getElementById("answer2nd").value;
+        // // var score = [];
+        // computer adds the figures
+        // var sumRandomNumbers = first + second;
+
+
+        // if ans1 result is 2 : append 1 to score 
+        // Otherwise : append 0 to score
+        // if (ans1 == 2) 
+        //         score.push(1);
         
-//         else 
-//                 score.push(0);
+        // else 
+        //         score.push(0);
 
-//         if (ans2 == 3)
-//                 score. push(1);
-//         else 
-//                 score.push(0);
+        // if (ans2 == 3)
+        //         score. push(1);
+        // else 
+        //         score.push(0);
 
-//         var total = 0;
-//         for (var i in score) {
-//                 total += score[i];
-//         }
-//         alert("Your score is " + total + " out of 2");
+        // var total = 0;
+        // for (var i in score) {
+        //         total += score[i];
+        // }
+//         alert("Computer answer = "+ sumRandomNumbers + 
+//         "User answer = " + ans1);
 // }
 
 // // function to redirect to another page
